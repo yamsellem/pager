8dddc0f222c2923f8bceec7f4c191d94d0b44e7c
yamsellem <amsellem.yves@gmail.com>
2016-01-26 17:19:23 +0100

    fix channel.every, propagate rule.match errors, improve docs

--- test/flow.test.js
+++ test/flow.test.js
@@ -1,11 +1,12 @@
 "use strict";
 
-var assert = require('chai').assert,
+var chai   = require('chai').use(require('chai-datetime')),
     sinon  = require('sinon'),
     moment = require('moment'),
     Flow   = require('../lib/flow'),
     Action = require('../lib/action'),
-    Rule   = require('../lib/rules/rule');
+    Rule   = require('../lib/rules/rule'),
+    assert = chai.assert;
 
 describe('flow', function() {
 
@@ -41,7 +42,7 @@ describe('flow', function() {
             });
 
             done();
-        });
+        }).catch(done);
     });
 
     it('should queue task if immediate rule matches', function(done) {
@@ -60,7 +61,7 @@ describe('flow', function() {
             sinon.assert.calledWith(match, {id: 1}, {options: 'set'});
 
             done();
-        });
+        }).catch(done);
     });
 
     it('should not queue task if immediate rule does not match', function(done) {
@@ -73,13 +74,13 @@ describe('flow', function() {
             .channel('emerge')
             .comply(rule);
 
-        flow.message('yellow:submarine', {id: 1}).catch(function() {
+        flow.message('yellow:submarine', {id: 1}).then(function() {
             sinon.assert.notCalled(emit);
             sinon.assert.calledOnce(match);
             sinon.assert.calledWith(match, {id: 1}, {options: 'set'});
 
             done();
-        });
+        }).catch(done);
     });
 
     it('should queue task without matching postponed rule', function(done) {
@@ -98,7 +99,7 @@ describe('flow', function() {
             sinon.assert.notCalled(match);
 
             done();
-        });
+        }).catch(done);
     });
 
     it('should process task without matching immediate rule', function(done) {
@@ -116,7 +117,7 @@ describe('flow', function() {
 
         var task = {
             event: 'yellow:submarine',
-            name: 'emerge',
+            channel: 'emerge',
             context: {id: 1},
             date: moment().subtract(1, 'seconds').toDate()
         };
@@ -126,7 +127,7 @@ describe('flow', function() {
             sinon.assert.called(fire);
 
             done();
-        });
+        }).catch(done);
     });
 
     it('should not process task if postponed rule timer not reached', function(done) {
@@ -144,16 +145,16 @@ describe('flow', function() {
 
         var task = {
             event: 'yellow:submarine',
-            name: 'emerge',
+            channel: 'emerge',
             context: {id: 1},
             date: moment().add(5, 'seconds').toDate()
         };
-        flow.process(task).catch(function() {
+        flow.process(task).then(function() {
             sinon.assert.notCalled(match);
             sinon.assert.notCalled(fire);
 
             done();
-        });
+        }).catch(done);
     });
 
     it('should process task if postponed rule timer reached', function(done) {
@@ -171,7 +172,7 @@ describe('flow', function() {
 
         var task = {
             event: 'yellow:submarine',
-            name: 'emerge',
+            channel: 'emerge',
             context: {id: 1},
             date: moment().add(5, 'seconds').toDate()
         };
@@ -183,13 +184,12 @@ describe('flow', function() {
             sinon.assert.called(fire);
 
             done();
-        });
+        }).catch(done);
     });
 
     it('should process task without action if postponed rule not match', function(done) {
         var flow = new Flow({options: 'set'}),
             rule = new Rule({match: function() { return false; }}),
-            match = sinon.spy(rule, 'match'),
             action = new Action(),
             fire = sinon.spy(action, 'fire')
 
@@ -201,23 +201,22 @@ describe('flow', function() {
 
         var task = {
             event: 'yellow:submarine',
-            name: 'emerge',
+            channel: 'emerge',
             context: {id: 1},
             date: moment().add(5, 'seconds').toDate()
         };
 
         this.clock.tick(5001);
-        flow.process(task).catch(function() {
+        flow.process(task).then(function() {
             sinon.assert.notCalled(fire);
 
             done();
-        });
+        }).catch(done);
     });
 
     it('should process task action if postponed rule matches', function(done) {
         var flow = new Flow({options: 'set'}),
             rule = new Rule(),
-            match = sinon.spy(rule, 'match'),
             action = new Action(),
             fire = sinon.spy(action, 'fire');
 
@@ -229,9 +228,9 @@ describe('flow', function() {
 
         var task = {
             event: 'yellow:submarine',
-            name: 'emerge',
+            channel: 'emerge',
             context: {id: 1},
-            date: new Date()
+            date: moment().add(5, 'seconds').toDate()
         };
 
         this.clock.tick(5001);
@@ -240,14 +239,13 @@ describe('flow', function() {
             sinon.assert.calledWith(fire, task.context, {options: 'set'});
 
             done();
-        });
+        }).catch(done);
     });
 
     it('should process task action repeat channel', function(done) {
         var flow = new Flow({options: 'set'}),
             emit = sinon.spy(flow, 'emit'),
             rule = new Rule(),
-            match = sinon.spy(rule, 'match'),
             action = new Action(),
             fire = sinon.spy(action, 'fire');
 
@@ -259,28 +257,52 @@ describe('flow', function() {
 
         var task = {
             event: 'yellow:submarine',
-            name: 'emerge',
+            channel: 'emerge',
             context: {id: 1},
-            date: new Date(),
+            date: moment().add(5, 'seconds').toDate(),
             flag: 'available'
         };
         var processMoment = moment().add(5001, 'millisecond'),
-            nextProcessMoment = processMoment.add(5, 'seconds');
+            nextProcessMoment = processMoment.add(4999, 'millisecond');
 
         this.clock.tick(5001);
         flow.process(task).then(function() {
             sinon.assert.called(fire);
             sinon.assert.calledWith(fire, task.context, {options: 'set'});
-            sinon.assert.calledOnce(emit);
-            sinon.assert.calledWith(emit, 'acknowledge', {
-                channel: 'emerge',
-                event: 'yellow:submarine',
-                context: task.context,
-                date: nextProcessMoment.toDate(),
-                flag: 'available'
-            });
+            assert.equalDate(task.date, nextProcessMoment.toDate());
 
             done();
-        });
+        }).catch(done);
+    });
+
+    it('should process all task action', function(done) {
+        var flow = new Flow({options: 'set'}),
+            firstAction = new Action(),
+            secondAction = new Action(),
+            firstFire = sinon.spy(firstAction, 'fire'),
+            secondFire = sinon.spy(secondAction, 'fire');
+
+        flow.subscribe('yellow:submarine')
+            .channel('emerge')
+            .every(5)
+            .trigger(firstAction, secondAction);
+
+        var task = {
+            event: 'yellow:submarine',
+            channel: 'emerge',
+            context: {id: 1},
+            date: moment().add(5, 'seconds').toDate(),
+            flag: 'available'
+        };
+
+        this.clock.tick(5001);
+        flow.process(task).then(function() {
+            sinon.assert.called(firstFire);
+            sinon.assert.called(secondFire);
+            sinon.assert.calledWith(firstFire, task.context, {options: 'set'});
+            sinon.assert.calledWith(secondFire, task.context, {options: 'set'});
+
+            done();
+        }).catch(done);
     });
 });
